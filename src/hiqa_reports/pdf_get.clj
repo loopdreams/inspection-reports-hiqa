91deb7b8908be888681a6a574c46ad2985ca47d5
Eoin Carney <eoincarney0@gmail.com>
2023-12-06 20:37:36 +0000

    process for updating pdfs

--- src/hiqa_reports/pdf_get.clj
+++ src/hiqa_reports/pdf_get.clj
@@ -5,10 +5,16 @@
    [clojure.java.io :as io]
    [clojure.set :as set]
    [clojure.string :as str]
-   [hiqa-reports.hiqa-register :refer [hiqa-reg-tbl pdf-matcher]]))
+   [hiqa-reports.hiqa-register :refer [hiqa-reg-DB pdf-matcher scrape-all-reports output-register-file register-tbl]]))
 
 (def destination-dir "inspection_reports/")
 (def report-urls-list-dir "resources/report_urls_list/")
+
+(def report-list
+  (->> (:reports hiqa-reg-DB)
+       (map edn/read-string)
+       flatten
+       (remove nil?)))
 
 (defn- fetch-pdf!
   "makes an HTTP request and fetches the binary object"
@@ -18,7 +24,7 @@
       (:body req))))
 
 (defn- save-pdf!
-  "downloads and stores the photo on disk"
+  "downloads and stores the pdf to disk"
   [pdf-link]
   (let [p (fetch-pdf! pdf-link)]
     (when (not (nil? p))
@@ -26,17 +32,11 @@
         (with-open [w (io/output-stream (str destination-dir filename))]
           (.write w p))))))
 
-(def report-list
-  (->> (:reports hiqa-reg-tbl)
-       (map edn/read-string)
-       flatten
-       (remove nil?)))
-
-(defn today-date []
+(defn- today-date []
   (.format (java.text.SimpleDateFormat. "yyyyMMdd")
            (java.util.Date.)))
 
-(defn print-reports-list-at-date [report-list & date]
+(defn- print-reports-list-at-date [report-list & date]
   (let [d (if date (first date) (today-date))]
     (let [fname (str report-urls-list-dir d "_report_list.txt")]
       (if (.exists (io/file fname))
@@ -44,44 +44,30 @@
         (let [data (str/join "\n" report-list)]
           (spit fname data))))))
 
-(defn most-recent-reports-file []
+(defn- most-recent-reports-file []
   (->> (file-seq (io/file report-urls-list-dir))
        rest
        sort
        last
        str))
 
-(comment
-  (print-reports-list-at-date (take 5 report-list))
-  (print-reports-list-at-date (take 6 report-list) "20231209"))
-
-(defn diff-reports [report-list & reports-file]
+(defn- diff-reports [report-list & reports-file]
   (let [file (if reports-file (first reports-file) (most-recent-reports-file))]
     (let [on-disk (str/split-lines (slurp file))]
-      (set/difference (set on-disk) (set report-list)))))
+      (into [] (set/difference (set on-disk) (set report-list))))))
 
+(defn- update-pdfs! [& reports-file]
+  (pmap save-pdf! (diff-reports report-list reports-file)))
+
+;; update the DB
 (comment
-  (diff-reports (take 5 report-list)))
-  
+  (scrape-all-reports register-tbl output-register-file)
 
-(comment
-  (count report-list)
-  (time (pmap save-pdf! report-list)))
+  (update-pdfs!))
 
-;; 3,153 Reports
-;;
-;; 500K size avg ...
-;;
-;; Around 1.5 GB total estimated
-;;
-;; Actual reports fetched: 3,098
-;;
-;; Sucess rate 98%
-;;
-;; Actual size on disk 1.18GB
 
-;; Getting extra info about the reports from filenames
 
+;; Additional analysis fns
 (def filename-date-matcher #"(\d{1,2})-(\w+)-(\d{4})")
 
 (def report-list-by-year
