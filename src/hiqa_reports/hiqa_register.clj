96f30c6b6cb6775ebc65a080f839e0e6ad1c324c
Eoin Carney <eoincarney0@gmail.com>
2023-12-07 01:42:43 +0000

    tidying up

--- src/hiqa_reports/hiqa_register.clj
+++ src/hiqa_reports/hiqa_register.clj
@@ -2,6 +2,7 @@
   (:require [tablecloth.api :as tc]))
 
 (def register-file "resources/disability_register.csv")
+(def output-register-file "outputs/hiqa_register_with_report_urls.csv")
 
 (def register-tbl
   (tc/dataset register-file {:key-fn keyword}))
@@ -10,7 +11,7 @@
 
 (def pdf-matcher #"[a-zA-Z0-9./_-]+.pdf")
 
-(defn scrape-report-urls [center-url]
+(defn- scrape-report-urls [center-url]
   (let [page (slurp center-url)
         pdf-matches? (re-seq pdf-matcher page)]
     (when (seq pdf-matches?)
@@ -22,11 +23,11 @@
       (tc/write! csv-out)))
 
 (comment
-  (time (scrape-all-reports register-tbl "outputs/hiqa_register_with_report_urls.csv")))
+  (time (scrape-all-reports register-tbl output-register-file)))
 
 ;; Elapsed time: 1101310.326875 msecs
 ;;
 
 
-(def hiqa-reg-tbl
+(def hiqa-reg-DB
   (tc/dataset "resources/hiqa_register_with_report_urls.csv" {:key-fn keyword}))
